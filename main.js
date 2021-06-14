4b78818d39f2c1e5e1e95a153161fa2b72a8beb5
kwalorz <kmwalorz@gmail.com>
2021-06-14 18:38:38 -0400

    working on javascript logic

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("generate_quote");
+const creditScore = document.getElementById("credit_score");
+const vehicleValue = document.getElementById("vehicle-value");
+const downPayment = document.getElementById("down_payment");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
