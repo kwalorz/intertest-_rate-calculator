5e70cdd4a7a43e26e394d97f1398d50ca7e70fc2
kwalorz <kmwalorz@gmail.com>
2021-06-15 16:42:33 -0400

    adding javascript logic

--- main.js
+++ main.js
@@ -7,10 +7,23 @@ const downPayment = document.querySelector("#down_payment");
 const tradeIn = document.querySelector("#trade");
 const leaseLength = document.querySelector("#lease_length");
 const quote = document.querySelector(".payment_quote");
+const [vehicleErr, downPaymentErr, tradeInErr] =
+  document.querySelectorAll(".error-message");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   let interestRate;
+  const errMessage =
+    "*The value you have entered is invalid. Must contain no special characters, letters, and be a positive number";
+
+  if (
+    vehicleValue.value < 0 ||
+    typeof /^\d*$/.test(vehicleValue.value) === "NaN"
+  ) {
+    vehicleErr.style.display = "block";
+    vehicleErr.textContent = `${errMessage}`;
+  }
+
   if (creditScore.value === "300-579") interestRate = 1.1;
   if (creditScore.value === "580-699") interestRate = 1.08;
   if (creditScore.value === "700-749") interestRate = 1.06;
